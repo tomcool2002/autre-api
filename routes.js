8018bfbab98fe63b16d3008209a00155ba5bba6f
tomcool2002 <32969040+tomcool2002@users.noreply.github.com>
2022-11-20 17:49:34 -0500

    test

--- routes.js
+++ routes.js
@@ -3,6 +3,16 @@ var routes = function(app){
         res.send("salut");
     })
     console.log("received get");
+
+    app.post("/",function(req,res){
+        if(!req.body.name || !req.body.score){
+            console.log("erreur");
+            return res.send({"status" : "error", "message": "missing parameters"});
+        }
+        else{
+            return res.send(req.body);
+        }
+    })
 };
 
 module.exports = routes;
