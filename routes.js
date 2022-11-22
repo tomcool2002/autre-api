d7c17a7eda2b6c27aa9eca6fab42f5e1716629bf
tomcool2002 <32969040+tomcool2002@users.noreply.github.com>
2022-11-22 17:19:29 -0500

    finish

--- routes.js
+++ routes.js
@@ -1,9 +1,7 @@
 const fs = require("fs");
 const JSONStream = require("jsonstream");
 
-
-
-let filepath = "data.json"
+let filepath = "data.json";
 
 var routes = function (app) {
   app.get("/", function (req, res) {
@@ -15,23 +13,15 @@ var routes = function (app) {
         try {
           const data = JSON.parse(jsonString);
           console.log(data);
-          if (data != null) {
-            data.sort(function (a, b) {
-              return b.score - a.score;
-            });
-            let BestScore = [];
-            for (let compteur = 0; compteur < 10; compteur++) {
-              //console.log(data[compteur]);
-              BestScore[compteur] = data[compteur];
-            }
-            res.send(BestScore);
+          data.sort(function (a, b) {
+            return b.score - a.score;
+          });
+          let BestScore = [];
+          for (let compteur = 0; compteur < 10; compteur++) {
+            //console.log(data[compteur]);
+            BestScore[compteur] = data[compteur];
           }
-          else{
-            console.log("oops");
-            res.send("oops");
-          }
-
-
+          res.send(BestScore);
         } catch (error) {
           console.log("error parsing JSON", err);
         }
@@ -39,36 +29,50 @@ var routes = function (app) {
     });
   });
 
-
-
   app.post("/", function (req, res) {
     console.log("received post");
     if (!req.body.name || !req.body.score) {
       console.log("erreur");
       return res.send({ status: "error", message: "missing parameters" });
     } else {
-        let jsonwriter = JSONStream.stringify();
-        let file = fs.createWriteStream(filepath);
-        jsonwriter.pipe(file);
+      fs.readFile(filepath, "utf-8", (err, jsonString) => {
+        if (err) {
+          res.send(err);
+        } else {
+          try {
+            const data = JSON.parse(jsonString);
+            //console.log(data);
 
+            let newScore = {
+              name: req.body.name,
+              score: req.body.score,
+            };
 
-        let newScore = {
-            "name": req.body.name,
-            "score": req.body.score
+            let jsonwriter = JSONStream.stringify();
+            let file = fs.createWriteStream(filepath);
+
+            jsonwriter.pipe(file);
+            data.forEach((score) =>{
+                jsonwriter.write(score);
+            })
+            jsonwriter.write(newScore);
+            jsonwriter.end();
+
+          } catch (error) {
+            console.log("error parsing JSON", err);
+          }
         }
+      });
 
-        jsonwriter.write(newScore);
-        jsonwriter.end();
-
-        //let newScoreString = JSON.stringify(newScore);
-        // fs.appendFile(filepath,newScoreString,(err)=>{
-        //     if(err){
-        //         console.log(err);
-        //     }
-        //     else{
-        //         console.log("we good");
-        //     }
-        // });
+      //let newScoreString = JSON.stringify(newScore);
+      // fs.appendFile(filepath,newScoreString,(err)=>{
+      //     if(err){
+      //         console.log(err);
+      //     }
+      //     else{
+      //         console.log("we good");
+      //     }
+      // });
       return res.send(req.body);
     }
   });
