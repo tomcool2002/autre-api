6599daa724dc0c7798c249e7e4d2dcb60ebcdd02
tomcool2002 <32969040+tomcool2002@users.noreply.github.com>
2022-11-22 16:21:51 -0500

    get good

--- routes.js
+++ routes.js
@@ -1,8 +1,34 @@
+const fs = require('fs');
+
 var routes = function(app){
     app.get("/",function(req,res){
-        res.send("salut");
+        console.log("received get");
+        fs.readFile('./data.json','utf-8',(err,jsonString)=>{
+            if(err){
+                res.send(err);
+            }else{
+                try {
+                const data = JSON.parse(jsonString);
+                data.sort(function(a,b){
+                    return b.score - a.score;
+                });
+                let BestScore = [];
+                for(let compteur = 0; compteur < 10; compteur++){ 
+                    console.log(data[compteur]);
+                    BestScore[compteur] = data[compteur]; 
+                }
+
+
+                res.send(BestScore);
+                } catch (error) {
+                    console.log("error parsing JSON",err);
+                }
+
+            }
+
+        })
     })
-    console.log("received get");
+
 
     app.post("/",function(req,res){
         if(!req.body.name || !req.body.score){
