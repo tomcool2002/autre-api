f79d7d7aae72326622afe4e07a7858fef97489c9
tomcool2002 <32969040+tomcool2002@users.noreply.github.com>
2022-11-22 16:57:52 -0500

    gros ts

--- routes.js
+++ routes.js
@@ -1,44 +1,77 @@
-const fs = require('fs');
-
-var routes = function(app){
-    app.get("/",function(req,res){
-        console.log("received get");
-        fs.readFile('./data.json','utf-8',(err,jsonString)=>{
-            if(err){
-                res.send(err);
-            }else{
-                try {
-                const data = JSON.parse(jsonString);
-                data.sort(function(a,b){
-                    return b.score - a.score;
-                });
-                let BestScore = [];
-                for(let compteur = 0; compteur < 10; compteur++){ 
-                    console.log(data[compteur]);
-                    BestScore[compteur] = data[compteur]; 
-                }
+const fs = require("fs");
+const JSONStream = require("jsonstream");
 
 
-                res.send(BestScore);
-                } catch (error) {
-                    console.log("error parsing JSON",err);
-                }
 
+let filepath = "data.json"
+
+var routes = function (app) {
+  app.get("/", function (req, res) {
+    console.log("received get");
+    fs.readFile(filepath, "utf-8", (err, jsonString) => {
+      if (err) {
+        res.send(err);
+      } else {
+        try {
+          const data = JSON.parse(jsonString);
+          console.log(data);
+          if (data != null) {
+            data.sort(function (a, b) {
+              return b.score - a.score;
+            });
+            let BestScore = [];
+            for (let compteur = 0; compteur < 10; compteur++) {
+              //console.log(data[compteur]);
+              BestScore[compteur] = data[compteur];
             }
+            res.send(BestScore);
+          }
+          else{
+            console.log("oops");
+            res.send("oops");
+          }
 
-        })
-    })
 
-
-    app.post("/",function(req,res){
-        if(!req.body.name || !req.body.score){
-            console.log("erreur");
-            return res.send({"status" : "error", "message": "missing parameters"});
+        } catch (error) {
+          console.log("error parsing JSON", err);
         }
-        else{
-            return res.send(req.body);
+      }
+    });
+  });
+
+
+
+  app.post("/", function (req, res) {
+    console.log("received post");
+    if (!req.body.name || !req.body.score) {
+      console.log("erreur");
+      return res.send({ status: "error", message: "missing parameters" });
+    } else {
+        let jsonwriter = JSONStream.stringify();
+        let file = fs.createWriteStream(filepath);
+        jsonwriter.pipe(file);
+
+
+        let newScore = {
+            "name": req.body.name,
+            "score": req.body.score
         }
-    })
+
+        jsonwriter.write(newScore);
+        jsonwriter.end();
+
+        //let newScoreString = JSON.stringify(newScore);
+        // fs.appendFile(filepath,newScoreString,(err)=>{
+        //     if(err){
+        //         console.log(err);
+        //     }
+        //     else{
+        //         console.log("we good");
+        //     }
+        // });
+      return res.send(req.body);
+    }
+  });
 };
 
 module.exports = routes;
